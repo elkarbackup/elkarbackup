1a9562151c2b3b098e33b10fb990a40e484d905d
blaskurain <blaskurain>
2013-03-20 15:24:28 +0000

    Reduce visible main menu flickering.

---
+++ src/Binovo/ElkarBackupBundle/Resources/public/js/base.js
@@ -0,0 +1,9 @@
+require(['dojo', 'dojo/parser', 'dijit/form/Button', 'dijit/MenuBar', 'dijit/MenuBarItem', 'dijit/PopupMenuBarItem', 'dijit/DropDownMenu', 'dijit/MenuItem', 'dojo/NodeList-manipulate', 'dojo/ready'],
+function(dojo, parser, Button, MenuBar, MenuBarItem, PopuMenuBarItem, DropDownMenu, MenuItem, NodeList, ready) {
+    ready(function() {
+              parser.parse().then(function(){
+                                      dojo.destroy('menuplaceholder');
+                                      dojo.setAttr(dojo.byId('menu'), 'style', '');
+                                  });
+          });
+});
