5d4db2f39d56a6982db79574184de4d972c90135
Victor Laskurain <blaskurain@binovo.es>
2013-05-17 07:59:35 +0200

    Ask confirmation when deleting a job in a client. Use while fonts when showing errors.

---
+++ src/Binovo/ElkarBackupBundle/Resources/public/js/edit-client.js
@@ -0,0 +1,18 @@
+/**
+ * @copyright 2012,2013 Binovo it Human Project, S.L.
+ * @license http://www.opensource.org/licenses/bsd-license.php New-BSD
+ */
+require(['dojo', 'dojo/ready'],
+function(dojo, ready){
+    ready(function() {
+              dojo.query('.delete-job')
+                  .connect('onclick',
+                           function (e){
+                               var msg;
+                               msg = dojo.getAttr(e.target, 'data-bnv-message');
+                               if (confirm(msg)) {
+                                   dojo.query(dojo.getAttr(e.target, 'data-bnv-job-id')).remove();
+                               }
+                           });
+          });
+});
