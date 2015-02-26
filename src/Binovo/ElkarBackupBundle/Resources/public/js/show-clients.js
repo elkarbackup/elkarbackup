ef4548ef551ed97c4fcb1c155a5e83f1ef8649cb
Xabi Ezpeleta <xezpeleta@gmail.com>
2015-02-26 16:40:22 +0000

    Show clients: run-now button on job rows (#36)

--- src/Binovo/ElkarBackupBundle/Resources/public/js/show-clients.js
+++ src/Binovo/ElkarBackupBundle/Resources/public/js/show-clients.js
@@ -2,11 +2,11 @@
  * @copyright 2012,2013 Binovo it Human Project, S.L.
  * @license http://www.opensource.org/licenses/bsd-license.php New-BSD
  */
-require(['dojo', 'dojo/ready'],
-function(dojo, ready){
+ require(['dojo', 'dojo/fx/Toggler', 'dojo/ready'],
+ function(dojo, Toggler, ready){
     ready(function() {
               dojo.query('.delete-job,.delete-client')
-                  .connect('onsubmit',
+                  .on('submit',
                            function (e){
                                var msg;
                                msg = dojo.getAttr(e.target, 'data-bnv-message');
@@ -14,8 +14,25 @@ function(dojo, ready){
                                    return true;
                                } else {
                                    e.preventDefault();
-                                   return false;    
+                                   return false;
                                }
                            });
+
+                           dojo.query('.runNow')
+                            .on('click',
+                           function (e){
+                               var args = {
+                                 form: e.target.form,
+                                 handleAs: 'text',
+                                 load: function(data) {
+                                   dojo.place('<div class="controls help-block alert alert-success">' + data + '</div>', dojo.byId('legend'), 'after');
+                                 },
+                                 error: function(data) {
+                                   dojo.place('<div class="controls help-block alert alert-danger">' + data + '</div>', dojo.byId('legend'), 'after');
+                                 }
+                               };
+                               console.log(args);
+                               dojo.xhrPost(args);
+                               });
           });
 });
