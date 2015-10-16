bcd269b099ad083129e32cd84fa25f71276d0af0
Xabi Ezpeleta <xezpeleta@gmail.com>
2015-10-16 15:12:54 +0000

    Updated to Symfony 2.7

--- app/autoload.php
+++ app/autoload.php
@@ -1,15 +1,12 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
+/**
+ * @var ClassLoader $loader
+ */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-}
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
