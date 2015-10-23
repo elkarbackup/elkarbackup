bcb5d2566099878c266af01eec69c4bac9292ce4
Adrián <lesauz@gmail.com>
2015-10-23 13:39:39 +0200

    Update ConfigureNodeCommand
    Added
     - node creation
     - aliases management
     - node connexion
     - alias creation

--- src/Binovo/ElkarTahoeBundle/Command/ConfigureNodeCommand.php
+++ src/Binovo/ElkarTahoeBundle/Command/ConfigureNodeCommand.php
@@ -23,73 +23,139 @@ class ConfigureNodeCommand extends LoggingCommand
             ->addArgument('s.H', InputArgument::OPTIONAL, '[client] Shares happy(H)')
             ->addArgument('s.N', InputArgument::OPTIONAL, '[client] Shares total(N)')
             ->addArgument('nname', InputArgument::OPTIONAL, '[node] Nodes nickname')
-            ->setDescription('Configures the existent Tahoe client node.');
+            ->setDescription('Configures the Tahoe client node.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $context = array('source' => 'ConfigureTahoeNodeCommand');
+        $context = array('source' => 'CreateTahoeNodeCommand');
 
-        //$clientNodeDir = $this->container->getParameter('tahoe_node_dir');
-        $clientNodeDir = '/var/lib/elkarbackup/.tahoe/';
-        $nodeConfigFile = $clientNodeDir . 'tahoe.cfg';
+        $tahoeAlias = 'tahoe'; //tahoe binary path
+
+        //Node creation
+        if( is_dir('.tahoe/') )
+            $this->info('Tahoe node exists');    
+        else {
+            $command = $tahoeAlias . ' create-client';
+            $commandOutput  = array();
+            $status         = 0;
+            exec($command, $commandOutput, $status);
+            if (0 != $status) {
+                $this->err('Error creating Tahoe node: ' . implode("\n", $commandOutput));
+                return $status;
+            }
+            $this->info('Tahoe node created ~/.tahoe/ ');     
+        }
+
+        //Node configuration
+        $nodeConfigFile = '.tahoe/tahoe.cfg';
 
         $attr = array();
         // [node]
         if ( !($attr['nickname'] = $input->getArgument('nname')) ) $attr['nickname'] = 'elkarbackup_node';
         // [client]
         $attr['introducer.furl'] = $input->getArgument('i.furl');
+
         if ( !($attr['shares.needed'] = $input->getArgument('s.K')) ) $attr['shares.needed'] = 3;
         if ( !($attr['shares.happy'] = $input->getArgument('s.H')) ) $attr['shares.happy'] = 7;
         if ( !($attr['shares.total'] = $input->getArgument('s.N')) ) $attr['shares.total'] = 10;
 
-        if(file_exists($nodeConfigFile) === true) {
+        if(file_exists($nodeConfigFile)) {
             if(is_writeable($nodeConfigFile) ) {
                 try {
                     $content = file_get_contents($nodeConfigFile);
-
                     $keys = array_keys($attr);
                     foreach ($keys as $key) {
                         $oldLine = '';
                         $newLine = $key . ' = ' . $attr[$key];
 
                         $i=strpos($content, ($key . ' =') );
-                        if($content[$i-1] == '#') $i--;                      
+                        if('introducer.furl' == $key) {
+                            $j=$i+strlen('introducer.furl = ');
+                            $oldFurl = ''; 
+                        }                     
+                        if('#' == $content[$i-1]) $i--;                      
                         for(;$i<strlen($content);$i++) {
-                            if($content[$i] == "\n") break;
+                            if("\n" == $content[$i]) break;
+                            if('introducer.furl' == $key and $i == $j) {
+                                $oldFurl = $oldFurl . $content[$i];
+                                $j++;
+                            }
                             $oldLine = $oldLine . $content[$i];
                         }
-
+                        if('introducer.furl' == $key) $oldFurlLine = $oldLine;
                         $content = str_replace($oldLine, $newLine, $content);
                     }
 
-                    if($out = file_put_contents($nodeConfigFile, $content) > 0) {
-                        $this->info('Success on tahoe node configuration');
-                        return 0;
-                    }
-                    else {
+                    if(file_put_contents($nodeConfigFile, $content) > 0) {
+                        $this->info('Node configuration set');
+                    } else {
                         $commandOutput = 'Error while writing file';
                         $this->err('Error configuring tahoe node: ' . $commandOutput);
                         return 1;
                     }
-                }
-                catch(Exception $e) {
+                } catch(Exception $e) {
                     $commandOutput = 'Error : '.$e;
                     $this->err('Error configuring tahoe node: ' . $commandOutput);
                     return 1;
                 }
-            }
-            else {
+            } else {
                 $commandOutput = 'No permission to write on the file';
                 $this->err('Error configuring tahoe node: ' . $commandOutput);
                 return 1;
             }
-        }
-        else {
+        } else {
             $commandOutput = 'File not found. Make sure the client node exists and the path is correct.';
             $this->err('Error configuring tahoe node:  ' . $commandOutput);
             return 1;
         }
+
+        //Manage aliases
+        if($oldFurl != $attr['introducer.furl']) {
+
+            $aliasesFile = '.tahoe/private/aliases';
+            if(file_exists($aliasesFile))
+            {
+                date_default_timezone_set("UTC");
+                $newName = date("Y-m-d_H:i:s", time());
+                $newName = 'old_aliases_' . $newName . 'Z'; //zulu time (utc)
+                if(is_writeable($nodeConfigFile))
+                    file_put_contents($aliasesFile, 'introducer.furl = ' . $oldFurl . "\n", FILE_APPEND);
+                $command = 'mv ' . $aliasesFile . ' .tahoe/private/' . $newName;
+                $commandOutput  = array();
+                $status         = 0;
+                exec($command, $commandOutput, $status);
+                if (0 != $status) {
+                    $this->err('Error saving old aliases: ' . implode("\n", $commandOutput));
+                    return $status;
+                }
+                $this->info('Old aliases saved in ~/.tahoe/private/' . $newName);
+                //New file will be automatically created when a new alias is created
+            }
+        }
+
+        //Launch daemon - Node connexion
+        $command = $tahoeAlias . ' restart'; //works even if it was not running
+        $commandOutput  = array();
+        $status         = 0;
+        exec($command, $commandOutput, $status);   
+        if (0 != $status) {
+            $this->err('Error starting Tahoe node: ' . implode("\n", $commandOutput));
+            return $status;
+        }
+        $this->info('Tahoe node started: ' . implode("\n", $commandOutput));
+
+        //Create elkarbackup directory in the tahoe grid
+        $command = $tahoeAlias . ' create-alias elkarbackup:';
+        $commandOutput  = array();
+        $status = 0;
+        exec($command, $commandOutput, $status);
+        if (0 == $status) {
+            $this->info('New alias created [ elkarbackup: ]');
+        }
+
+        return 0;
+
     }
 
     protected function getNameForLogs()
