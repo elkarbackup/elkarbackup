0b58fbd6ca1e9db91454e306dc2a84e8ea04cbef
Adrián <lesauz@gmail.com>
2015-10-27 09:08:50 +0100

    Update stopCommand
    · remove unused imports (use)

--- src/Binovo/ElkarTahoeBundle/Command/StopCommand.php
+++ src/Binovo/ElkarTahoeBundle/Command/StopCommand.php
@@ -7,9 +7,7 @@
 namespace Binovo\ElkarTahoeBundle\Command;
 
 use Binovo\ElkarBackupBundle\Lib\LoggingCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class StopCommand extends LoggingCommand
