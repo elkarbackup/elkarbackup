cfcbe713e305a607e4e4a511397d190618f8ec90
xezpeleta <xepeleta@gmail.com>
2015-12-14 22:04:37 +0000

    Fix chmod for dirs with spaces

--- makepackage.sh
+++ makepackage.sh
@@ -55,7 +55,7 @@ cp -a changelog changelog.Debian copyright .debian/usr/share/doc/elkarbackup
 gzip -f --best .debian/usr/share/doc/elkarbackup/changelog
 gzip -f --best .debian/usr/share/doc/elkarbackup/changelog.Debian
 # ensure directory permissions are right
-find .debian -type d | xargs chmod 755
+find .debian -type d | xargs -I {} chmod 755 "{}"
 # set initial values for parametres
 sed -i 's#backup_dir:.*#backup_dir: /var/spool/elkarbackup/backups#'       .debian/etc/elkarbackup/parameters.yml
 sed -i 's#database_name:.*#database_name: elkarbackup#'                    .debian/etc/elkarbackup/parameters.yml
