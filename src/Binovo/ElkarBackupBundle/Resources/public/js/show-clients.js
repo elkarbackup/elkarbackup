1e215cecdc2e17bedf9deca046cc25feea596d75
xezpeleta <xezpeleta@gmail.com>
2016-01-15 13:55:22 +0000

    Added jquery function for POST requests

--- src/Binovo/ElkarBackupBundle/Resources/public/js/show-clients.js
+++ src/Binovo/ElkarBackupBundle/Resources/public/js/show-clients.js
@@ -2,6 +2,23 @@
  * @copyright 2012,2013 Binovo it Human Project, S.L.
  * @license http://www.opensource.org/licenses/bsd-license.php New-BSD
  */
+
+
+  function postRequest(url, params) {
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: params,
+      success: function(response) {
+        console.log(response.length);
+        // Provisional workaround until we change deleteClientAction response to a message string instead of a GET
+        if (response.length < 80){
+          $("#legend").after('<div class="controls help-block alert alert-success">' + response + '</div>');
+        }
+      }
+    });
+  };
+
  require(['dojo', 'dojo/fx/Toggler', 'dojo/ready'],
  function(dojo, Toggler, ready){
     ready(function() {
