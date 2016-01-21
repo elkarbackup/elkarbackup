69b672e67f79921827d2919d2e456317a0839382
Antton Rodriguez <antton.rodriguez@gmail.com>
2016-01-21 12:05:46 +0100

    clean part of base javascript

--- src/Binovo/ElkarBackupBundle/Resources/public/js/base.js
+++ src/Binovo/ElkarBackupBundle/Resources/public/js/base.js
@@ -2,8 +2,31 @@
  * @copyright 2012,2013 Binovo it Human Project, S.L.
  * @license http://www.opensource.org/licenses/bsd-license.php New-BSD
  */
-require(['dojo', 'dojo/parser', 'dijit/form/Button', 'dijit/PopupMenuBarItem', 'dijit/DropDownMenu', 'dojo/NodeList-manipulate', 'dojo/ready', 'dojo/dom-class', 'dojo/query!css2', 'dijit/registry'],
-function(dojo, parser, Button, PopuMenuBarItem, DropDownMenu, NodeList, ready, domClass, query, registry) {
+
+ function postRequest(url, params) {
+   $.ajax({
+     type: "POST",
+     url: url,
+     data: params,
+     success: function(response) {
+       if (response.msg){
+         okMsg(response.msg);
+
+         if (response.action){
+           if (response.data){
+             // Call to callback
+             window[response.action].apply(null, response.data);
+           }
+         }
+       }
+     }
+   });
+ };
+
+
+
+require(['dojo', 'dojo/parser', 'dijit/form/Button', 'dijit/PopupMenuBarItem', 'dijit/DropDownMenu', 'dojo/NodeList-manipulate', 'dojo/ready', 'dojo/dom-class', 'dijit/registry'],
+function(dojo, parser, Button, PopuMenuBarItem, DropDownMenu, NodeList, ready, domClass, registry) {
     ready(function() {
               parser.parse().then(function(){
 
