c5a29d9ecf658d464934eb79e442141077c9590f
Xabi <xezpeleta@gmail.com>
2018-02-27 17:48:12 +0100

    Integrate TravisCI (#272)
    * Integrate TravisCI
    * Just a small test
    * Change TravisCI badge url

--- src/Binovo/ElkarBackupBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Binovo/ElkarBackupBundle/Tests/Controller/DefaultControllerTest.php
@@ -14,8 +14,8 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/login');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Log in")')->count() > 0);
     }
 }
