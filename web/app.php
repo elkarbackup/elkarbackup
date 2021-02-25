0464a125ee21b2a2a57b926ab6affcca3fcd119b
Uxue <u.anabitarte@gmail.com>
2021-02-25 08:46:54 +0100

    Deprecated class loader component deleted

--- web/app.php
+++ web/app.php
@@ -20,7 +20,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
--- web/app_dev.php
+++ web/app_dev.php
@@ -25,7 +25,6 @@ Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
