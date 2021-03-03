948235ce6a667aa188fc2d236f22620571caaf65
Uxue <u.anabitarte@gmail.com>
2021-03-03 16:18:38 +0100

    delete extra

--- extra/windows/DeleteSnapshotCUmountB.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-C:\ElkarBackup\vss.exe B:
-cscript /nologo C:\ElkarBackup\snapshot.vbs /command:DeleteSnapshot /snapshot:C:\ElkarBackup\ids.txt
-del C:\ElkarBackup\ids.txt
--- extra/windows/MakeSnapshotCMountB.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-cscript /nologo C:\ElkarBackup\snapshot.vbs /command:CreateSnapshot /volume:C /mount:B > C:\ElkarBackup\ids.txt
