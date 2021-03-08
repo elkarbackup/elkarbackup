37865bf8b57ff933b1bd432f972ded84c6f1a72f
Uxue <u.anabitarte@gmail.com>
2021-03-08 08:32:56 +0100

    Install WebProfilerBundle to use Data Collector Translator

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
