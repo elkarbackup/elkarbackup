c35c665c55ab9d3ea7fb23a5cc24e3f9af57a7ac
Uxue <u.anabitarte@gmail.com>
2021-03-17 10:18:41 +0100

    Undo unnecessary change from previous commit

--- src/Migrations/LegacyBackupDirAwareInterface.php
+++ src/Migrations/LegacyBackupDirAwareInterface.php
@@ -4,6 +4,6 @@ namespace App\Migrations;
 interface LegacyBackupDirAwareInterface
 {
     //backup_dir parameter is not used since 2018 versions, but it is necessary to migrate from older versions
-    public function setLegacyBackupDir($backupDir): void;
+    public function setLegacyBackupDir(string $backupDir): void;
 }
 
