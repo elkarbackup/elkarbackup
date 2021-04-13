5d091381795f110e4a33e9aa442b8ae8eac595cb
Uxue <u.anabitarte@gmail.com>
2021-04-13 10:27:47 +0200

    Add preScript and postScript setters

--- src/Api/DataTransformers/ClientInputDataTransformer.php
+++ src/Api/DataTransformers/ClientInputDataTransformer.php
@@ -2,10 +2,12 @@
 namespace App\Api\DataTransformers;
 
 use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
-use App\Api\Dto\ClientInput;
+use ApiPlatform\Core\Exception\InvalidArgumentException;
+use ApiPlatform\Core\Serializer\AbstractItemNormalizer;
 use App\Entity\Client;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use \Exception;
 
 class ClientInputDataTransformer implements DataTransformerInterface
 {
@@ -30,8 +32,8 @@ class ClientInputDataTransformer implements DataTransformerInterface
         $client->setQuota($data->getQuota());
         $client->setDescription($data->getDescription());
         $client->setIsActive($data->getIsActive());
-//      $client->addPreScript($preScripts);
-//      $client->addPostScript($postScripts);
+        $this->setPreScripts($client, $data->getPreScript());
+        $this->setPostScripts($client, $data->getPostScript());
         $client->setMaxParallelJobs($data->getMaxParallelJobs());
         $client->setOwner($this->getOwner($data->getOwner()));
         $client->setSshArgs($data->getSshArgs());
@@ -39,27 +41,64 @@ class ClientInputDataTransformer implements DataTransformerInterface
         $client->setRsyncLongArgs($data->getRsyncLongArgs());
         return $client;
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function supportsTransformation($data, string $to, array $context = []): bool
     {
-        // in the case of an input, the value given here is an array (the JSON decoded).
-        // if it's a book we transformed the data already
         if ($data instanceof Client) {
             return false;
         }
-        
+
         return Client::class === $to && null !== ($context['input']['class'] ?? null);
     }
-    
+
     private function getOwner($id): ?User
     {
         $repository = $this->entityManager->getRepository('App:User');
         $query = $repository->createQueryBuilder('c');
         $query->where($query->expr()->eq('c.id', $id));
         return $query->getQuery()->getOneOrNullResult();
+
+    private function setPreScripts($client, $preScripts): void
+    {
+        $repository = $this->entityManager->getRepository('App:Script');
+        $query = $repository->createQueryBuilder('s');
+        foreach ($preScripts as $script) {
+            $query = $repository->createQueryBuilder('s');
+            $query->where($query->expr()->eq('s.id', $script));
+            $result = $query->getQuery()->getOneOrNullResult();
+            if (null != $result) {
+                if ($result->getIsClientPre()) {
+                    $client->addPreScript($result);
+                }else {
+                    throw new InvalidArgumentException(sprintf('Script "%s" is not a client pre script', $result->getId()));
+                }
+            } else {
+                throw new InvalidArgumentException(sprintf('Script "%s" does not exist', $script));
+            }
+        }
+    }
+
+    private function setPostScripts($client, $postScripts): void
+    {
+        $repository = $this->entityManager->getRepository('App:Script');
+        $query = $repository->createQueryBuilder('s');
+        foreach ($postScripts as $script) {
+            $query = $repository->createQueryBuilder('s');
+            $query->where($query->expr()->eq('s.id', $script));
+            $result = $query->getQuery()->getOneOrNullResult();
+            if (null != $result) {
+                if ($result->getIsClientPost()) {
+                    $client->addPostScript($result);
+                }else {
+                    throw new InvalidArgumentException(sprintf('Script "%s" is not a client post script', $result->getId()));
+                }
+            } else {
+                throw new InvalidArgumentException(sprintf('Script "%s" does not exist', $script));
+            }
+        }
     }
 }
 
