e3f928d6809bf8c69030b83d0fb58a25c6f15e82
Uxue <u.anabitarte@gmail.com>
2021-04-19 14:08:02 +0200

    "Get policies" functionality

---
+++ src/Api/Dto/PolicyOutput.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Api\Dto;
+
+class PolicyOutput
+{
+    private $id;
+    private $description;
+    private $name;
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    
+}
+
