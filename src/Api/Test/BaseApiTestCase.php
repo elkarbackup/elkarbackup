84736a4a4327d6a2eb4eb4e52c11d8d42f028851
Uxue <u.anabitarte@gmail.com>
2021-05-04 15:33:37 +0200

    Change base test to just authenticate

--- src/Api/Test/BaseApiTestCase.php
+++ src/Api/Test/BaseApiTestCase.php
@@ -2,27 +2,16 @@
 namespace App\Api\Test;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use App\Entity\User;
-use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
 
 class BaseApiTestCase extends ApiTestCase
 {
-    use RecreateDatabaseTrait;
-    
-    protected function createUser(): User
+    protected function authenticate(): Client
     {
-        $user = new User();
-        $user->setUsername('root');
-        $user->setEmail('root@localhost');
-        $user->setRoles(array('ROLE_ADMIN'));
-        $user->setSalt(md5(uniqid(null, true)));
-        $encoder = self::bootKernel()->getContainer()->get('security.encoder_factory')->getEncoder($user);
-        $password = $encoder->encodePassword('root', $user->getSalt());
-        $user->setPassword($password);
-        $em = self::$container->get('doctrine')->getManager();
-        $em->persist($user);
-        $em->flush();
-        return $user;
+        return static::createClient([], [
+            'auth_basic' => ['root', 'root'],
+            'base_uri' => 'http://127.0.0.1'
+        ]);
     }
 }
 
