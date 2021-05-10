99b518ac73aa57fbfad26b7a565a570de05d26eb
Uxue <u.anabitarte@gmail.com>
2021-05-10 15:41:42 +0200

    Load fixtures when running tests

--- run-tests.sh
+++ run-tests.sh
@@ -7,4 +7,5 @@ $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
 sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
+$DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
