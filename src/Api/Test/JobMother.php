05cc94abb96f5b9aef70ddab90306d9111ebdef7
Uxue <u.anabitarte@gmail.com>
2021-05-11 14:22:32 +0200

    Create job tests

--- src/Api/Test/JobMother.php
+++ src/Api/Test/JobMother.php
@@ -18,29 +18,134 @@ class JobMother
         ];
         return $json;
     }
-}
 
-// {
-//     "backupLocation": 1,
-//     "client": 0,
-//     "description": "string",
-//     "exclude": "string",
-//     "include": "string",
-//     "isActive": true,
-//     "minNotificationLevel": 400,
-//     "name": "string",
-//     "notificationsEmail": "string",
-//     "notificationsTo": [
-//         "owner"
-//     ],
-//     "path": "string",
-//     "policy": 1,
-//     "postScripts": [
-//         "string"
-//     ],
-//     "preScripts": [
-//         "string"
-//     ],
-//     "token": "string",
-//     "useLocalPermissions": true
-// }
+    public static function withBackupLocation(string $jobName, int $backupLocationId): array
+    {
+        $json = [
+            'backupLocation' => $backupLocationId,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+
+    public static function withClient (string $jobName, int $clientId): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => $clientId,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+    
+    public static function withNotificationLevel(string $jobName, int $minNotificationLevel): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => $minNotificationLevel,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+
+    public static function withNotificationsEmail(string $jobName, string $notificationsEmail): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsEmail' => $notificationsEmail,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+
+    public static function withNotificationsTo(string $jobName, array $notificationsTo): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => $notificationsTo,
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+
+    public static function withPolicy(string $jobName, int $policyId): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => $policyId,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+    public static function withPostScripts(String $jobName, array $postScripts): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'postScripts' => $postScripts,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+
+    public static function withPreScripts(String $jobName, array $preScripts): array
+    {
+        $json = [
+            'backupLocation' => 1,
+            'client' => 1,
+            'isActive' => true,
+            'minNotificationLevel' => 400,
+            'name' => $jobName,
+            'notificationsTo' => ['owner'],
+            'path' => '/some/default/path',
+            'policy' => 1,
+            'preScripts' => $preScripts,
+            'useLocalPermissions' => true
+        ];
+        return $json;
+    }
+}
--- src/Api/Test/JobTest.php
+++ src/Api/Test/JobTest.php
@@ -16,5 +16,119 @@ class JobTest extends BaseApiTestCase
         $this->assertJsonContains($jobJson);
         $this->assertJobHydraContext();
     }
+
+    //public function testCreateJobAllParameters(): void
+
+    public function testCreateJobInvalidBackupLocation(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withBackupLocation($jobName, self::UNEXISTING_ID);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect backup location id');
+    }
+    public function testCreateJobInvalidClient(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withClient($jobName, self::UNEXISTING_ID);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect client id');
+    }
+
+    public function testCreateJobInvalidNotificationLevel(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withNotificationLevel($jobName, 333);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect notification level (0, 200, 300, 400, 1000)');
+    }
+
+    public function testCreateJobInvalidNotificationsEmail(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withNotificationsEmail($jobName, 'invalid email');
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect notification email address');
+    }
+
+    public function testCreateJobInvalidNotificationsTo(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withNotificationsTo($jobName, ['invalid']);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect notifications to argument (owner, admin, email)');
+    }
+
+    public function testCreateJobInvalidPolicy(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withPolicy($jobName, self::UNEXISTING_ID);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Incorrect policy id');
+    }
+
+    public function testCreateJobInvalidPostScript(): void
+    {
+        $httpClient = $this->authenticate();
+        $scriptId = $this->getScriptId($httpClient, 'script_not_job_post');
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withPostScripts($jobName, [$scriptId]);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Script "'.$scriptId.'" is not a job post script');
+    }
+
+    public function testCreateJobInvalidPreScript(): void
+    {
+        $httpClient = $this->authenticate();
+        $scriptId = $this->getScriptId($httpClient, 'script_not_job_pre');
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withPreScripts($jobName, [$scriptId]);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Script "'.$scriptId.'" is not a job pre script');
+    }
+
+    public function testCreateJobUnexistentPostScript(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withPostScripts($jobName, [self::UNEXISTING_ID]);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Script "'.self::UNEXISTING_ID.'" does not exist');
+    }
+
+    public function testCreateJobUnexistentPreScript(): void
+    {
+        $httpClient = $this->authenticate();
+        $jobName = $this->createJobName();
+        $jobJson = JobMother::withPreScripts($jobName, [self::UNEXISTING_ID]);
+        $this->postJob($httpClient, $jobJson);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertHydraError('Script "'.self::UNEXISTING_ID.'" does not exist');
+    }
+    
 }
 
