ee907cfc8feda2be7e7e428cf0bab32bccf30810
Uxue <u.anabitarte@gmail.com>
2021-05-11 09:01:27 +0200

    Update ClientMother

--- src/Api/Test/ClientMother.php
+++ src/Api/Test/ClientMother.php
@@ -3,31 +3,73 @@ namespace App\Api\Test;
 
 class ClientMother
 {
-    public static function base(string $clientName, int $owner): array
+    public static function named(string $clientName): array
     {
         $json = [
         'isActive' => true,
         'maxParallelJobs' => 1,
         'name' => $clientName,
-        'owner' => $owner,
+        'owner' => 1,
         'quota' => -1
         ];
         return $json;
     }
     
-    public static function withMaxParallelJobs(string $clientName, int $owner, int $maxParallelJobs): array
+    public static function withAllParameters(
+        string $clientName, 
+        int $owner, 
+        string $description, 
+        bool $isActive, 
+        int $maxParallelJobs, 
+        array $postScripts, 
+        array $preScripts, 
+        int $quota, 
+        string $rsyncLongArgs, 
+        string $rsyncShortArgs, 
+        string $sshArgs, 
+        string $url
+    ): array {
+        $json = [
+            'description' => $description,
+            'isActive' => $isActive,
+            'maxParallelJobs' => $maxParallelJobs,
+            'name' => $clientName,
+            'owner' => $owner,
+            'postScripts' => $postScripts,
+            'preScripts' => $preScripts,
+            'quota' => $quota,
+            'rsyncLongArgs' => $rsyncLongArgs,
+            'rsyncShortArgs' => $rsyncShortArgs,
+            'sshArgs' => $sshArgs,
+            'url' => $url
+        ];
+        return $json;
+    }
+    
+    public static function withMaxParallelJobs(string $clientName, int $maxParallelJobs): array
     {
         $json = [
             'isActive' => true,
             'maxParallelJobs' => $maxParallelJobs,
+            'name' => $clientName,
+            'owner' => 1,
+            'quota' => -1
+        ];
+        return $json;
+    }
+    
+    public static function withOwner (string $clientName, int $owner): array
+    {
+        $json = [
+            'isActive' => true,
+            'maxParallelJobs' => 1,
             'name' => $clientName,
             'owner' => $owner,
             'quota' => -1
         ];
         return $json;
     }
-    
-    public static function withPostScripts(string $clientName, int $owner, array $postScripts): array
+    public static function withPostScripts(string $clientName, array $postScripts): array
     {
         $json = [
         'isActive'        => true,
@@ -40,7 +82,7 @@ class ClientMother
         return $json;
     }
 
-    public static function withPreScripts(string $clientName, int $owner, array $preScripts): array
+    public static function withPreScripts(string $clientName, array $preScripts): array
     {
         $json = [
             'isActive'        => true,
