8cb57f4191fc97a9129a73debc33144763278046
Xabi <xezpeleta@gmail.com>
2021-05-18 22:49:12 +0200

    Run with sudo if sudo exists

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
-sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
+SUDO="$(which sudo)" && "${SUDO}" --preserve-env $DIR/bin/console elkarbackup:create_admin || $DIR/bin/console elkarbackup:create_admin
 $DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
