5497db6f9376eef64aed72f18e82a8d974f5af0b
uanabitarte <u.anabitarte@gmail.com>
2021-05-19 16:00:50 +0200

    Update run-tests

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,6 @@ $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
 sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
-mkdir /tmp/elkarbackup-tests
-mkdir /tmp/elkarbackup-tests/uploads
+mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
