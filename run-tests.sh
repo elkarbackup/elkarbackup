8b4b43359ed6f1e21972cba270e44e07e39eda58
uanabitarte <55280468+uanabitarte@users.noreply.github.com>
2021-05-19 16:15:10 +0200

    Script fixture's script directory changed (#565) (#566)
    Use temporary dir for test upload files and save uploaded scripts there

--- run-tests.sh
+++ run-tests.sh
@@ -7,5 +7,6 @@ $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
 sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
+mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
