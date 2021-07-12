5888b1eff64c655e6c5e1bb71e48f23d84fdc0ee
Xabi Ezpeleta <xezpeleta@gmail.com>
2021-07-12 22:08:24 +0200

    Remove sudo
    Should fix an issue with CI tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
-sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
+$DIR/bin/console elkarbackup:create_admin 
 mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
