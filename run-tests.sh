1bfeeb9e343eb27b43f9e4ec9e7548647fdad8cb
Andreas Thienemann <andreas@bawue.net>
2025-07-06 20:07:50 +0200

    fix(tests): Improve Actions workflow.
    * Use current version of the docker related actions.
    * Ensure `run_test.sh` will always exit on error.
    * Group the script output so that GitHub Actions can fold parts of the
      testsuite logs.

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,11 @@ echo "Database setup"
 $DIR/bin/console doctrine:database:drop --force || /bin/true
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
+
+echo "General setup"
 sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
 mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
+
+echo "Running unit tests"
 $DIR/bin/phpunit "${@:1}"
